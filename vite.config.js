1f64e0a42d24aefc150391a43a43b40ed813af19
Salomon Samion <salamon.salamon0@gmail.com>
2024-10-13 14:59:45 +0000

    13.10.24

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync('C:/Users/sala0986/porkbunKey/private.key.pem'), // Updated to .pem
+      cert: fs.readFileSync('C:/Users/sala0986/porkbunKey/domain.cert.pem'), // Updated to .pem
+    },
+    host: '0.0.0.0', // Allow access from the network (optional)
+    port: 443, // Specify the port
+  },
+  
+});
